a54794cf8876d053c29d6824b21a2feb2ab28a6d
hoan <quanghoan1992@gmail.com>
2015-12-30 18:08:49 +0700

    fix some bug

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,14 +1,8 @@
 class Test < ActiveRecord::Base
-  has_many :grade
   belongs_to :subject
   belongs_to :user
   has_many :answers, dependent: :destroy
   accepts_nested_attributes_for :answers
   has_many :questions, through: :subjects
   validates :subject_id, presence: true 
-
-  def score
-    answers = self.answers
-    answers.select{|answer| answer.is_correct}.count
-  end
 end
