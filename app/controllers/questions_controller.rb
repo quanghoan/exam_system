1a1f1d8d389e2ad4c78cc4dbdb17fa56565cf7c4
hoan <quanghoan1992@gmail.com>
2016-01-07 16:33:08 +0700

    fix bug new user

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -12,17 +12,14 @@ class QuestionsController < ApplicationController
     @subjects = Subject.all
   end
 
-  def edit
-    @question = Question.find(params[:id])
-    @subjects = Subject.all
-    @subject = @question.subject
-  end
-
   def create    
-    index = params[:answers_attributes][:i][:correct_answer]
-    correct_index = params[:answers_attributes][index]
-    correct_index = correct_index.update({correct_answer: true})
-    params[:answers_attributes].delete(:i)
+    if !params[:answers_attributes].nil? && !params[:answers_attributes][:i].nil?
+      index = params[:answers_attributes][:i][:correct_answer]
+      correct_index = params[:answers_attributes][index]
+      correct_index = correct_index.update({correct_answer: true})
+      params[:answers_attributes].delete(:i)
+    end  
+    @subjects = Subject.all
     @question = Question.new(question_params)
     if @question.save
       flash[:success] = "Added a question."
@@ -36,16 +33,6 @@ class QuestionsController < ApplicationController
     Question.find(params[:id]).destroy
     flash[:success] = "question deleted"
     redirect_to questions_url
-  end
-
-  def update
-    @question = Question.find(params[:id])    
-    if @question.update_attributes(question_params)
-      flash[:success] = "question updated."
-      redirect_to questions_url
-    else
-      render 'edit'
-    end
   end
 
   private
